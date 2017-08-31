038e72a4b8764b256f0eeaf12ac19e9033376544
alex <alex940131@gmail.com>
2017-08-31 12:03:35 +0800

    edit game day

--- client/dist/js/bundle.js
+++ client/dist/js/bundle.js
@@ -57650,7 +57650,7 @@
 	  mongoURI: 'mongodb://admin:admin123@ds115583.mlab.com:15583/digit-dev',
 	  cookieKey: 'fvjdfnvonvcofunveuyhbciwenx',
 	  jwtSecret: 'afdsfvfdsgsdfsdf',
-	  gameDay: '20170931'
+	  gameDay: '20170831'
 	};
 
 
